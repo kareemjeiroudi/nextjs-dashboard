c1705c91aa7f7e03ae66d36e88c4bacc4176ef44
Kareem Jeiroudi <kareemjeiroudi@gmail.com>
2024-05-14 15:01:57 +0200

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,29 @@
-import '@/app/ui/global.css';
+import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 
-export default function RootLayout({ children, }: { children: React.ReactNode; }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-    <body className={`${inter.className} antialiased`}>{children}</body>
+      <head>
+        <title>Next.Js Tutorial</title>
+        <meta
+          name="description"
+          content="This page is deploy by dev github.com/kareemjeiroudi to learn Next.Js for future projects."
+        />
+        <meta property="og:title" content="Next.Js Tutorial" />
+        <meta
+          property="og:description"
+          content="This page is deploy by dev github.com/kareemjeiroudi to learn Next.Js for future projects."
+        />
+        <meta property="og:image" content="/opengraph-image.png" />
+        <meta name="keywords" content="keyword1, keyword2, keyword3" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
