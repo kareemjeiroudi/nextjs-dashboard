7b1ce3b637bc574e48302efada85f7a89b79a193
Kareem Jeiroudi <kareemjeiroudi@gmail.com>
2024-05-14 12:42:02 +0200

    Error handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,17 +1,25 @@
-import Form from '@/app/ui/invoices/edit-form';
-import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import Form from "@/app/ui/invoices/edit-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const [invoice, customers] = await Promise.all([fetchInvoiceById(params.id), fetchCustomers()]);
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(params.id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
       <Breadcrumbs
         breadcrumbs={[
-          { label: 'Invoices', href: '/dashboard/invoices' },
+          { label: "Invoices", href: "/dashboard/invoices" },
           {
-            label: 'Edit Invoice',
+            label: "Edit Invoice",
             href: `/dashboard/invoices/${invoice.id}/edit`,
             active: true,
           },
--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,12 @@
-const styleguide = require('@vercel/style-guide/prettier');
+const styleguide = require("@vercel/style-guide/prettier");
 
 module.exports = {
   ...styleguide,
-  plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+  plugins: [...styleguide.plugins, "prettier-plugin-tailwindcss"],
+  semi: true,
+  trailingComma: "none",
+  singleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  tabWidth: 2,
 };
